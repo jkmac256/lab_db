f3a10615078ea879a9f08ec247e80696c4f8f3ae
jkmac256 <114290836+jkmac256@users.noreply.github.com>
2025-08-01 13:52:33 +0300

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,30 +1,16 @@
 # backend/database.py
-
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.orm import sessionmaker
 
-# ✅ Load the DATABASE_URL from environment variables
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
 
-# ✅ Function to create tables
 def create_tables():
-    from models import User, TestResult, TestRequest, Equipment  # Add all your models here
+    from models import Laboratory, User, Equipment, Patient, TestRequest, TestResult,
     Base.metadata.create_all(bind=engine)
-
-# ✅ Dependency function for DB session
-def get_db():
-    db: Session = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
