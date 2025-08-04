56ae5205c67d5c2282c31ae1686e81d35a50d9ad
jkmac256 <114290836+jkmac256@users.noreply.github.com>
2025-08-04 18:20:09 +0300

    Update google_cloud.py

--- backend/routes/google_cloud.py
+++ backend/routes/google_cloud.py
@@ -1,41 +1,39 @@
 import os
+import json
+import tempfile
 from google.cloud import storage
 from google.oauth2 import service_account
 from fastapi import UploadFile
 from datetime import datetime
 from dotenv import load_dotenv
 
-# Load environment variables
 load_dotenv()
 
-# Your GCS bucket name
 BUCKET_NAME = "medicallab-results-buckect"
 
-# Get the relative path from .env
-relative_path = os.getenv("GCS_CREDENTIALS")  
 
-# Resolve the absolute path safely (even on Render)
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # backend/routes
-GCS_CREDENTIALS_PATH = os.path.abspath(os.path.join(BASE_DIR, "..", relative_path))
+gcs_creds_json = os.getenv("GOOGLE_APPLICATION_CREDENTIALS_JSON")
 
-# Initialize GCS client
-credentials = service_account.Credentials.from_service_account_file(GCS_CREDENTIALS_PATH)
+
+with tempfile.NamedTemporaryFile(mode='w+', delete=False, suffix='.json') as temp:
+    temp.write(gcs_creds_json)
+    temp.flush()
+    temp_path = temp.name 
+
+
+credentials = service_account.Credentials.from_service_account_file(temp_path)
 client = storage.Client(credentials=credentials)
 bucket = client.bucket(BUCKET_NAME)
 
 def upload_file_to_gcs(file: UploadFile, request_id: int, make_public=False) -> str:
     filename = f"test-results/{datetime.utcnow().strftime('%Y%m%d%H%M')}_{file.filename}"
     blob = bucket.blob(filename)
-
-    # Upload the file
     blob.upload_from_file(file.file, content_type=file.content_type)
 
-    # Make public if requested
     if make_public:
         blob.make_public()
         return blob.public_url
-    else:
-        return filename
+    return filename
 
 
 def generate_signed_url(blob_path: str, expiration_minutes: int = 30) -> str:
